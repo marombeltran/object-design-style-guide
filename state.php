cb61bb7a919148892f5fba05d6e7222b072e8809
Marlon Beltran <mbeltran@marencoventiladores.com>
2023-06-28 14:58:36 -0600

    State

--- state.php
+++ state.php
@@ -18,6 +18,52 @@ class Foo
         $this->someNumber = 10;
         $this->someString =  'Hello, world';
     }
+
+    /* The data contained in an object is also known as its state. If that data is going to be
+     * hardcoded, as in the previous example, you might as well make it part of the property
+     * definition or define a constant for it.
+     */
+
+// 1.8 Defining constants
+// ^-----------------------------------------
+        // private const int someNumber = 10;
+        // private someString = 'Hello, world!';
+
 }
+
+
+
+/* On the other hand, if the initial value of a property should be variable, you can let the
+ * client provide a value for it as a constructor argument. By adding a parameter to the
+ * constructor, you force clients to provide a value when instantiating the class.
+ */
+
+
+// 1.9 Adding a constructor argument
+// ^-----------------------------------------
+
+class Fooo
+{
+    private int $someNumber;
+
+    public function __construct(int $initialNumber)
+    {
+        $this->someNumber = $initialNumber;
+    }
+}
+
+$object1 = new Fooo(); // doesn't work
+
+// This should work; it assigns the initial value of 20 to the someNumber
+// property of the new Foo instance.
+$object1 = new Fooo(10); 
+
+/* Marking the someNumber and someString properties as private makes them available
+ * to instances of Foo only. This is called scoping. Alternative scopes for properties are
+ * protected and public. By making a property public, you make it
+ * accessible to any client.
+ */
+
+
 
 
